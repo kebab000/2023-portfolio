7fb838137d9ece25b7201b54802d10096dbfd54b
kebab000 <dldytpq7010@gmail.com>
2023-06-28 12:16:52 +0900

    두번째 포폴 시작

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import "./assets/script/section1.js"
+import "./assets/script/section2.js"
+import "./assets/script/section3.js"
+import "./assets/script/section4.js"
+import "./assets/script/section5.js"
+import "./assets/script/ticket.js"
+// import "./style.scss";
+
