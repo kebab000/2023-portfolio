d79ee5080285b6b9878f4d5f57630da012c1019a
kebab000 <dldytpq7010@gmail.com>
2023-06-30 14:38:46 +0900

    23.06.30

--- src/assets/script/layout.js
+++ src/assets/script/layout.js
@@ -1,15 +1,51 @@
 let scrollTop = 0
 const layout1 = document.querySelector(".left .left__desc");
 const layout2 = document.querySelector(".right #ticket");
+const siteCount = document.querySelector(".site__count") 
+const phpCount = document.querySelector(".php__count"); 
+let count1 = 0;
+let count2 = 0;
 gsap.set(layout1, {autoAlpha: 1, y:-5100})
 gsap.set(layout2, {autoAlpha: 1, y:535})
 let scrollLayout = 0
 window.addEventListener("scroll", ()=>{
     scrollTop = window.pageYOffset || window.scrollY ||document.documentElement.scrollTop;
     scrollLayout = scrollTop * 0.5                        
+    siteCount.innerHTML = count1;
+    phpCount.innerHTML = count2; 
+
     //info
     document.querySelector(".scroll span").innerHTML = parseInt(scrollTop);
  
     gsap.to(layout1, { duration: 0.3, y: -5100 + scrollLayout });
+
+    if ( scrollTop == 6606 ){
+        layout2.style.transform = "translate(0px, 506px)";
+        count1++
+    
+    }
+    if ( scrollTop == 9505 ){
+        layout2.style.transform = "translate(0px, 490px)";
+        count1++
+    
+    }
+    if ( scrollTop == 13200 ){
+        layout2.style.transform = "translate(0px, 475px)";
+        count1++
+    
+    }
+    if ( scrollTop == 17145 ){
+        layout2.style.transform = "translate(0px, 460px)";
+        count1++
+    
+    }
+    if ( scrollTop == 20300 ){
+        layout2.style.transform = "translate(0px, 410px)";    
+        count2++
+    }
+    if ( scrollTop == 24420 ){
+        layout2.style.transform = "translate(0px, 395px)";    
+        count2++
+    }
     });
 
--- src/assets/script/section03.js
+++ src/assets/script/section03.js
@@ -89,8 +89,8 @@ ani3.to(".sec3-section3 .box",      {duration:0.3, x: 300})
 ani3.to(".sec3-section3 .box",      {duration:0.3, y: 300})
 ani3.to(".sec3-section3 .box",      {duration:0.3, x: 0})
 ani3.to(".sec3-section3 .box",      {duration:0.3, y: -150})
-ani3.to(".sec3-section3 .box",      {duration:0.5, width: "10vw", height: "60vh"})
-ani3.to(".sec3-section3 .box",      {duration:0.5, width: "25vw", height: "60vh"})
+ani3.to(".sec3-section3 .box",      {duration:1.5, width: "10vw", height: "33vw"})
+ani3.to(".sec3-section3 .box",      {duration:0.5, width: "25vw", height: "33vw"})
 ani3.to(".sec3-section3 .box h3",   {duration:0.5, autoAlpha:1, y: 0})
 ani3.to(".sec3-section3 .box p",    {duration:0.5, autoAlpha:1, y: 0})
 ani3.to(".sec3-section3 .box .btn", {duration:0.5, autoAlpha:1, y: 0})
@@ -124,7 +124,7 @@ ani4.to(".sec3-section4 .box span", {duration:0.5, autoAlpha:1})
 ani4.to(".sec3-section4 .box",      {duration:0.3, x: 200})
 ani4.to(".sec3-section4 .box",      {duration:0.3, y: 200})
 ani4.to(".sec3-section4 .box",      {duration:0.3, x: 0})
-ani4.to(".sec3-sect4on2 .box",      {duration:0.3, y: -50})
+ani4.to(".sec3-section4 .box",      {duration:0.3, y: -100})
 ani4.to(".sec3-section4 .box",      {duration:0.4, width: "25vw", height: "10vw"})
 ani4.to(".sec3-section4 .box",      {duration:0.5, width: "25vw", height: "auto"})
 ani4.to(".sec3-section4 .box h3",   {duration:0.5, autoAlpha:1, y: 0})
@@ -136,6 +136,80 @@ ani4.to(".sec3-section4 .box .mockup",  {duration:1.5, autoAlpha:1, x: 0})
 ScrollTrigger.create({
     animation: ani4,
     trigger: ".sec3-section4",
+    start: "top 100%",
+    end: "+=3500",
+    scrub: true,
+    pin: true, 
+    anticipatePin: 1,
+    // markers: true
+});
+
+// section3-section5
+gsap.set(".sec3-section5 .box", {autoAlpha: 1, width: "10vw", height: "10vw"})
+gsap.set(".sec3-section5 .box span", {autoAlpha: 0})
+gsap.set(".sec3-section5 .box h3", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section5 .box p", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section5 .box .btn", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section5 .box .qr", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section5 .box .mockup", {autoAlpha: 0, x: 100})
+
+const ani5 = gsap.timeline();
+ani5.to(".sec3-section5 .box",      {duration:0.5, width: "10vw", height: "10vw", autoAlpha:1})
+ani5.to(".sec3-section5 .box span", {duration:0.3, autoAlpha:1})
+// ani5.to(".sec3-section5 .box",      {duration:1.3, rotateZ: -720, x:0, y: 0})
+ani5.to(".sec3-section5 .box",      {duration:0.3, x: 300})
+ani5.to(".sec3-section5 .box",      {duration:0.3, y: 300})
+ani5.to(".sec3-section5 .box",      {duration:0.3, x: 0})
+ani5.to(".sec3-section5 .box",      {duration:0.3, y: 0})
+ani5.to(".sec3-section5 .box",      {duration:0.5, width: "25vw", height: "10vw"})
+ani5.to(".sec3-section5 .box",      {duration:0.5, width: "25vw", height: "auto"})
+ani5.to(".sec3-section5 .box h3",   {duration:0.5, autoAlpha:1, y: 0})
+ani5.to(".sec3-section5 .box p",    {duration:0.5, autoAlpha:1, y: 0})
+ani5.to(".sec3-section5 .box .btn", {duration:0.5, autoAlpha:1, y: 0})
+ani5.to(".sec3-section5 .box .qr",  {duration:0.5, autoAlpha:1, y: 0})
+ani5.to(".sec3-section5 .box .mockup",  {duration:1.5, autoAlpha:1, x: 0})
+// ani1.to(".sec3-section1 .box",      {duration:1, y: -10, autoAlpha:1})
+
+ScrollTrigger.create({
+    animation: ani5,
+    trigger: ".sec3-section5",
+    start: "top 150%",
+    end: "+=3500",
+    scrub: true,
+    pin: true, 
+    anticipatePin: 1,
+    markers: true
+});
+
+// box 6
+
+// section3-section6
+gsap.set(".sec3-section6 .box", {autoAlpha: 1, width: "10vw", height: "10vw"})
+gsap.set(".sec3-section6 .box span", {autoAlpha: 0})
+gsap.set(".sec3-section6 .box h3", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section6 .box p", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section6 .box .btn", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section6 .box .qr", {autoAlpha: 0, y: 10})
+gsap.set(".sec3-section6 .box .mockup", {autoAlpha: 0, x: -100})
+
+const ani6 = gsap.timeline();
+ani6.to(".sec3-section6 .box",      {duration:0, width: "10vw", height: "10vw", autoAlpha:1, x:0})
+ani6.to(".sec3-section6 .box span", {duration:0.5, autoAlpha:1})
+ani6.to(".sec3-section6 .box",      {duration:0.3, x: 200})
+ani6.to(".sec3-section6 .box",      {duration:0.3, y: 200})
+ani6.to(".sec3-section6 .box",      {duration:0.3, x: 0})
+ani6.to(".sec3-section6 .box",      {duration:0.3, y: -100})
+ani6.to(".sec3-section6 .box",      {duration:0.4, width: "25vw", height: "10vw"})
+ani6.to(".sec3-section6 .box",      {duration:0.5, width: "25vw", height: "auto"})
+ani6.to(".sec3-section6 .box h3",   {duration:0.5, autoAlpha:1, y: 0})
+ani6.to(".sec3-section6 .box p",    {duration:0.5, autoAlpha:1, y: 0})
+ani6.to(".sec3-section6 .box .btn", {duration:0.5, autoAlpha:1, y: 0})
+ani6.to(".sec3-section6 .box .qr",  {duration:0.5, autoAlpha:1, y: 0})
+ani6.to(".sec3-section6 .box .mockup",  {duration:1.5, autoAlpha:1, x: 0})
+
+ScrollTrigger.create({
+    animation: ani6,
+    trigger: ".sec3-section6",
     start: "top 100%",
     end: "+=3500",
     scrub: true,
